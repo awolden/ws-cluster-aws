44be9c9fb22a8fc5ce713340669710edbeeab493
Alex Wolden <woldena@ochin.org>
2016-08-20 13:16:33 -0700

    added healthcheck

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const WebSocket = require('ws');
 const hostname = require('os').hostname();
 const cluster = require('cluster');
 const CLUSTER_COUNT = 2;
+const http = require('http');
 
 if(cluster.isMaster){
   for(let i = 0; i < CLUSTER_COUNT; i++){
     cluster.fork();
   }
+
+  // healthcheck
+  const server = http.createServer(function(request, response) {
+    response.end('');
+  });
+  server.listen(8777);
+
 }else{
   const wss = WebSocket.Server({port: 8080});
   wss.on('connection', function connection(ws) {
